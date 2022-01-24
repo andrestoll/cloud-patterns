5336cf4aa4752ce25e3525470cb1b4f77434f782
André Stoll <andre.stoll@hotmail.com>
2022-01-24 15:16:28 +0100

    ServiceBus Message Writer

---
+++ MessagerApp/MessagerApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using MessagerApp;
+
+using IHost host = Host.CreateDefaultBuilder(args).Build();
+
+
+// Build a config object, using env vars and JSON providers.
+IConfiguration config = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
+
+
+// Get values from the config given their key and their target type.
+var queueName = config.GetRequiredSection("ServiceBus:QueueName").Get<string>();
+var connectionString = config.GetRequiredSection("ServiceBus:ConnectionString").Get<string>();
+
+var serviceBusMessenger = new ServiceBusMessager(connectionString, queueName);
+
+await serviceBusMessenger.SendMessage(50);
+
+await host.RunAsync();
---
+++ MessagerApp/MessagerApp/ServiceBusMessager.cs
@@ -0,0 +1,54 @@
+﻿using Azure.Messaging.ServiceBus;
+
+namespace MessagerApp
+{
+    public class ServiceBusMessager
+    {
+        // the client that owns the connection and can be used to create senders and receivers
+        ServiceBusClient client;
+
+        // the sender used to publish messages to the queue
+        ServiceBusSender sender;
+
+
+        public ServiceBusMessager(string connectionString, string queueName)
+        {
+            client = new ServiceBusClient(connectionString);
+            sender = client.CreateSender(queueName);
+        }
+
+        public async Task SendMessage(int numOfMessages)
+        {
+            // create a batch 
+            using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
+
+            for (int i = 1; i <= numOfMessages; i++)
+            {
+                // try adding a message to the batch
+                if (!messageBatch.TryAddMessage(new ServiceBusMessage($"Message {i}")))
+                {
+                    // if it is too large for the batch
+                    throw new Exception($"The message {i} is too large to fit in the batch.");
+                }
+            }
+
+            try
+            {
+                // Use the producer client to send the batch of messages to the Service Bus queue
+                await sender.SendMessagesAsync(messageBatch);
+                Console.WriteLine($"A batch of {numOfMessages} messages has been published to the queue.");
+            }
+            finally
+            {
+                // Calling DisposeAsync on client types is required to ensure that network
+                // resources and other unmanaged objects are properly cleaned up.
+                await sender.DisposeAsync();
+                await client.DisposeAsync();
+            }
+
+            Console.WriteLine("Press any key to end the application");
+            Console.ReadKey();
+        }
+
+    }
+}
